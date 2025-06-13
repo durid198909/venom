ee6a3485d9cc02648b7f8003a6329329555f70a2
durid198909 <tteto.iraq8@gmail.com>
2025-06-14 02:55:03 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        sw: './public/sw.js'
+      }
+    }
+  },
+  server: {
+    headers: {
+      'Service-Worker-Allowed': '/'
+    }
+  }
+});
